dec2887f3bc41254ed5debde694027569d2da59a
Michele Esposito <michele@esposito.codes>
2023-09-24 10:47:17 +0200

    refactor: better organize errors modules

--- src/keychain/signer/errors.rs
+++ src/keychain/signer/errors.rs
@@ -2,3 +2,13 @@
 pub enum SignerError {
   InvalidPrivateKey,
 }
+
+impl std::fmt::Display for SignerError {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      Self::InvalidPrivateKey => write!(f, "Invalid private key"),
+    }
+  }
+}
+
+impl std::error::Error for SignerError {}
--- src/keychain/vault/errors.rs
+++ src/keychain/vault/errors.rs
@@ -1,3 +1,5 @@
+use std::{error::Error, fmt::Display};
+
 use crate::{AccountError, SignerError};
 
 #[derive(Debug)]
@@ -13,6 +15,22 @@ pub enum VaultError {
   SafeDecrypt,
 }
 
+impl Display for VaultError {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      Self::ForbiddenWhileLocked => write!(f, "Forbidden while locked"),
+      Self::AccountCreation => write!(f, "Account creation error"),
+      Self::InvalidPassword => write!(f, "Invalid password"),
+      Self::InvalidMnemonic => write!(f, "Invalid mnemonic"),
+      Self::SignerCreation => write!(f, "Signer creation error"),
+      Self::KeyDerivation => write!(f, "Key derivation error"),
+      Self::AlreadyUnlocked => write!(f, "Already unlocked"),
+      Self::SafeCreation => write!(f, "Safe creation error"),
+      Self::SafeDecrypt => write!(f, "Safe decryption error"),
+    }
+  }
+}
+
 impl From<AccountError> for VaultError {
   fn from(_: AccountError) -> Self {
     Self::AccountCreation
@@ -24,3 +42,5 @@ impl From<SignerError> for VaultError {
     Self::SignerCreation
   }
 }
+
+impl Error for VaultError {}
